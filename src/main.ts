83046ca942c66bb04bf5aa1ab2d412551eaaa632
sarathradhakrishnan <sarathradhakrishnan@qburst.com>
2022-12-29 20:24:13 +0530

    swagger module added

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { CustomLoggerService } from './shared/custom-logger/custom-logger.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,17 @@ async function bootstrap() {
   const port = configService.get('configuration.port');
   const host = configService.get('configuration.host');
   const env = configService.get('configuration.env');
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('DocumentTitle')
+    .setDescription('Document description')
+    .setVersion('1.0')
+    .addTag('Tags')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api-docs', app, document);
 
   await app.listen(port, host);
   logger.log(`Server spun up at port=${port} with enviroment=${env}`);
